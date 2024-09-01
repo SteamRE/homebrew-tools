19b4babde842d99056435b163ebc10b1fa9e46be
Yaakov <git@yaakov.dev>
2024-09-01 16:37:37 +1000

    Fix Shellcheck indentation

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -5,23 +5,23 @@ CURRENT_VERSION=$(grep 'version "' Casks/depotdownloader.rb | cut -d '"' -f 2)
 
 if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]
 then
-    echo "DepotDownloader Cask is up to date."
-    exit 0
+  echo "DepotDownloader Cask is up to date."
+  exit 0
 fi
 
 if [ "$(uname)" = "Darwin" ]
 then
-    SED_INLINE="sed -i .tmp"
+  SED_INLINE="sed -i .tmp"
 else
-    SED_INLINE="sed -i"
+  SED_INLINE="sed -i"
 fi
 
 update_for_arch() {
-    BREW_ARCH=$1
-    FILE_ARCH=$2
+  BREW_ARCH=$1
+  FILE_ARCH=$2
 
-    FILE_SHA_256=$(curl -fSsL -o - "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_${LATEST_VERSION}/DepotDownloader-macos-${FILE_ARCH}.zip" | shasum -a 256 | awk '{print $1}')
-    $SED_INLINE "s/${BREW_ARCH}:\([ \t]*\)\"[0-9a-f]*\"/${BREW_ARCH}:\1\"${FILE_SHA_256}\"/g" Casks/depotdownloader.rb
+  FILE_SHA_256=$(curl -fSsL -o - "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_${LATEST_VERSION}/DepotDownloader-macos-${FILE_ARCH}.zip" | shasum -a 256 | awk '{print $1}')
+  $SED_INLINE "s/${BREW_ARCH}:\([ \t]*\)\"[0-9a-f]*\"/${BREW_ARCH}:\1\"${FILE_SHA_256}\"/g" Casks/depotdownloader.rb
 }
 
 $SED_INLINE "s/version \"${CURRENT_VERSION}\"/version \"${LATEST_VERSION}\"/" Casks/depotdownloader.rb
@@ -30,5 +30,5 @@ update_for_arch intel x64
 
 if [ "$(uname)" = "Darwin" ]
 then
-    rm Casks/*.tmp
+  rm Casks/*.tmp
 fi
